f8627a219e87e31858c3a50c2552e867ecc55c29
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-27 10:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const squaresList = Array.from(document.getElementsByClassName("square"));
+
+for(let square of squaresList) {
+	square.addEventListener("mouseover", changeOthersColor);
+	square.addEventListener("mouseleave", revertColors);
+}
+function changeOthersColor(event) {
+	// event.target.style.backgroundColor = "blue";
+	squaresList.forEach((element)=>{
+		if(element != event.target) {
+			element.classList.toggle("coffee");
+		}
+	})
+}
+function revertColors (event) {
+	// event.target.style.backgroundColor = "lavender"
+	squaresList.forEach((element)=> {
+		if(element.classList.contains("coffee")) {
+			element.classList.remove("coffee");
+		}
+	})
+}
